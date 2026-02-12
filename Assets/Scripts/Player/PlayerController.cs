e2059c611bf345a8a12ca8a48a51828470045dbb
Leon Twemlow <leon@twemlow.co.uk>
2026-02-12 11:36:24 +0000

    [Leon] Basic Player Controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("Movement speed in units per second.")]
+    public float moveSpeed = 5f;
+    public float runMultiplier = 1.5f;
+
+    [Tooltip("Smoothly rotate to face movement direction when moving.")]
+    public bool faceMovementDirection = true;
+
+    Rigidbody rb;
+    Vector3 inputDirection;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.freezeRotation = true;
+        }
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        inputDirection = new Vector3(h, 0f, v).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        if (rb != null)
+        {
+            float currentSpeed = moveSpeed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed *= runMultiplier;
+            }
+            Vector3 targetVelocity = inputDirection * currentSpeed;
+            rb.linearVelocity = new Vector3(targetVelocity.x, rb.linearVelocity.y, targetVelocity.z);
+        }
+        else
+        {
+            transform.position += inputDirection * (moveSpeed * Time.fixedDeltaTime);
+        }
+
+        if (faceMovementDirection && inputDirection.sqrMagnitude > 0.0001f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(inputDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 10f * Time.fixedDeltaTime);
+        }
+    }
+}
