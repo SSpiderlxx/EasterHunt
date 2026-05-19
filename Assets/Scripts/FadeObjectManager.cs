b9a9fc40911488d8e9a40933592acdaf443a3354
Leon Twemlow <leon@twemlow.co.uk>
2026-05-19 23:03:33 +0100

    Implement fade in/out functionality for objects on player trigger

--- Assets/Scripts/FadeObjectManager.cs
+++ Assets/Scripts/FadeObjectManager.cs
@@ -5,6 +5,30 @@ public class FadeObjectManager : MonoBehaviour
 {
     [SerializeField] FadeObject[] fadeObjects;
 
+    private bool objectsAreFaded;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!IsPlayer(other) || objectsAreFaded)
+        {
+            return;
+        }
+
+        FadeAllObjectsOut();
+        objectsAreFaded = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!IsPlayer(other) || !objectsAreFaded)
+        {
+            return;
+        }
+
+        FadeAllObjectsIn();
+        objectsAreFaded = false;
+    }
+
     public void FadeAllObjectsIn()
     {
         foreach (FadeObject obj in fadeObjects)
@@ -19,5 +43,10 @@ public class FadeObjectManager : MonoBehaviour
         {
             obj.FadeOut();
         }
+    }
+
+    private bool IsPlayer(Collider other)
+    {
+        return other.GetComponentInParent<PlayerController>() != null;
     }
 }
