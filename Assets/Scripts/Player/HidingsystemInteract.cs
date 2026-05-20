49116151238385a85a9d76f2e336316125a27719
Leon Twemlow <leon@twemlow.co.uk>
2026-05-21 00:51:16 +0100

    Refactor Hidingsystem class and add AIController metadata

--- Assets/Scripts/Player/HidingsystemInteract.cs
+++ Assets/Scripts/Player/HidingsystemInteract.cs
@@ -5,39 +5,6 @@ public class Hidingsystem : MonoBehaviour, IInteractable
 
     public bool CanInteract = true;
 
-    public Hide hidingmanager_script;
-
-    public void Interact()
-    {
-        if (CanInteract == true)
-        {
-            Ray ray = new Ray(transform.position, transform.forward);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 5))
-            {
-                if (hit.collider.CompareTag("Hide")) 
-                {
-
-                    CanInteract = false;
-                    hidingmanager_script.GoHide();
-
-                }
-            }
-        }
-    }
-
-
-
-
-}
-using UnityEngine;
-
-public class Hidingsystem : MonoBehaviour, IInteractable
-{
-
-    public bool CanInteract = true;
-
     public void Interact()
     {
         if (CanInteract == true)
